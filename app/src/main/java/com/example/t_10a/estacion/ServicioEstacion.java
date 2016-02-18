fa7993a9a11b56604c77435a8740878a9f5c4410
daton <rapidclimate@gmail.com>
2016-02-17 19:43:24 -0600

    Hola

--- app/src/main/java/com/example/t_10a/estacion/ServicioEstacion.java
+++ app/src/main/java/com/example/t_10a/estacion/ServicioEstacion.java
@@ -1,5 +1,7 @@
 package com.example.t_10a.estacion;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -13,15 +15,30 @@ public class ServicioEstacion {
 
     public String leerEstacion()throws Exception{
         String mensaje="nada todavia";
-      URL url=new URL("http://www.weatherlink.com/user/sierraguadalupe");
+      URL url=new URL("http://www.weatherlink.com/user/sierraguadalupe/index.php?view=summary&headers=0");
         URLConnection con= url.openConnection();
         InputStreamReader in=new InputStreamReader(con.getInputStream());
         BufferedReader reader=new BufferedReader(in);
+
         int numeroLineas=0;
-        while(reader.readLine()!=null){
-            numeroLineas++;
-            if(numeroLineas==155)mensaje=reader.readLine().toString();
+        boolean encontrada=false;
+        String lineaActual="nada";
+        while((lineaActual=reader.readLine())!=null){
+
+            if(encontrada && numeroLineas<=1){
+                System.out.println(lineaActual);
+                numeroLineas++;
+            }
+           if(lineaActual.contains("Outside Temp")){
+               encontrada=true;
+               System.out.println("SI LA ENCONTRO!!!");
+               numeroLineas++;
+           }
+
         }
+        System.out.println("este se ve una vez por estar afuera del while");
+
+
 
         return mensaje;
     }
